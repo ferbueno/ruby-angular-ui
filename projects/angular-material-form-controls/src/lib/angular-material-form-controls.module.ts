6125b4a4cef6c564711fa98f06d5685477b8baef
Víctor Andrés Martínez Valdés <victorandresmtz@gmail.com>
2018-07-25 17:08:01 -0500

    Setting up lib

---
+++ projects/angular-material-form-controls/src/lib/angular-material-form-controls.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AngularMaterialFormControlsComponent } from './angular-material-form-controls.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [AngularMaterialFormControlsComponent],
+  exports: [AngularMaterialFormControlsComponent]
+})
+export class AngularMaterialFormControlsModule { }
