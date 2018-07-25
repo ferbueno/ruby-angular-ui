8371793599d0482b591cddf7b4c9191704680406
Víctor Andrés Martínez Valdés <victorandresmtz@gmail.com>
2018-07-25 16:46:59 -0500

    Styles to login page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/material/material.module';
 
 import { LoginContainerComponent } from './login-container/login-container.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
@@ -14,6 +19,6 @@ import { LoginContainerComponent } from './login-container/login-container.compo
       }
     ])
   ],
-  declarations: [LoginContainerComponent]
+  declarations: [LoginContainerComponent, LoginFormComponent]
 })
 export class AuthModule { }
